d948cf8e829cf6032da57838d8f7e9ac9c0d4829
jackoPrytz <weaverjackop@gmail.com>
2022-06-29 10:02:41 -0300

    best of 5 game added for the console. script.js

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ function gameRound(userInput,compInput){
         return `It's a draw!! you both played ${userInput}`;
     }
     else if(userInput==="rock"&&compInput==="scissors"||userInput==="paper"&& compInput==="rock"||userInput==="scissors"&& compInput==="paper"){
-        return `Good job you won!!!`
+        return `User won`
     }
     else{
-        return `Bad luck the Computer won!!`
+        return `Computer won`
     }
 }
 // create a function called game that plays five rounds of gameTime
@@ -38,8 +38,34 @@ function gameRound(userInput,compInput){
             //repeat until first player gets to 3
             //Announce winner
 
+function game(){ 
+                let compScore=0;
+                let userScore=0;
+                let rounds= 1;
+        
+                while (compScore<3 && userScore<3) {
+                    console.log(`Round:${rounds}`)
+                    userInput=prompt('paper, scissors rock. Type your answer.',"rock");
+                    compInput=computerPlay();
 
-            
+                        let round = gameRound(userInput,compInput)
+                        if(round==="User won"){
+                            userScore++;
+                            console.log(`You won this round the score is You:${userScore} computer:${compScore}`)
+                        }else if(round==="Computer won"){
+                            compScore++;
+                            console.log(`Computer won this round the score is You: ${userScore} computer:${compScore}`)
+                        }else{
+                            console.log(`This round was a draw the score is You: ${userScore} computer:${compScore}`)
+                        }
+                        rounds++;
+                    }  
+                if(compScore>userScore){
+                    console.log("The computer won, better luck next time!!")
 
+                }   else{
+                    console.log("You won!! great job.")
+                }  
+            }
 
    
